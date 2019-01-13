a25082f3f6f101de7ef6f98e476801fb77412e42
Banana Viking <bananavikingcoder@gmail.com>
2019-01-13 12:19:27 -0600

    install pod Firebase/Auth

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -28,15 +28,7 @@ class RegisterViewController: UIViewController {
 
   
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
+        Fir
     } 
     
     
